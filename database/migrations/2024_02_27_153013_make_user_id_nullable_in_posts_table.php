d9d16f1a7eed12b64e42191dc6c4b0871f5dac6d
kotaroshimizu0611 <kotaroshimizu0611@gmail.com>
2024-02-28 14:33:14 +0900

    node.js

--- database/migrations/2024_02_27_153013_make_user_id_nullable_in_posts_table.php
+++ database/migrations/2024_02_27_153013_make_user_id_nullable_in_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+             $table->unsignedBigInteger('user_id')->nullable()->change();
         });
     }
 
